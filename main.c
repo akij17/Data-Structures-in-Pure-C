e2a42f4fbd2f52c261a3cbd7780e07b07fbd54b5
Harsh Gundecha <harsh.gundecha@gmail.com>
2018-11-05 22:51:03 +0530

    Update main.c
    added required code for BST

--- main.c
+++ main.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include "arraylist.h"
 #include "liststructures.h"
+#include "tree.h"
 
 int main() {
-    printf(
-            "What will you like to test?\n"
-            "1) List Structures\n"
-            "Enter your selection: "
-            );
-    int x; scanf("%d", &x);
-    switch(x){
-        case 1: listMenu();
-        break;
-
-    }
-    return 0;
+	int ch;
+	while(1)
+	{
+	    printf(
+	        "What will you like to test?\n"
+	        "1. List Structures\n"
+	        "2. Binary Search Tree\n"
+			"99. EXIT\n"
+			"Enter your Choice :\n"
+	    );
+		scanf("%d", &ch);
+	    switch(ch)
+		{
+	        case 1:
+				listMenu();
+	        break;
+	
+	        case 2:
+				tree_bst();
+	        break;
+			
+			case 99:
+				return 0;
+	    }
+	}
 }
